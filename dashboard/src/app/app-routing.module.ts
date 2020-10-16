b4a4aa21cb6f78727a34e9f6c60cabcbf5c52faa
Josh Haxton <haxtonjr@gmail.com>
2020-10-15 22:01:46 -0500

    Added in components, routing, and tabing

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BudgetComponent } from './budget/budget.component';
+import { GroceryComponent } from './grocery/grocery.component';
+import { WeightComponent } from './weight/weight.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'budget', component: BudgetComponent},
+  {path: '', redirectTo: 'budget', pathMatch: 'full'},
+  {path: 'grocery', component: GroceryComponent},
+  {path: 'weight', component: WeightComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BudgetComponent } from './budget/budget.component';
+import { GroceryComponent } from './grocery/grocery.component';
+import { WeightComponent } from './weight/weight.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BudgetComponent,
+    GroceryComponent,
+    WeightComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
