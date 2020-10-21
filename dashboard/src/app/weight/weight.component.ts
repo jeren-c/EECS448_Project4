4d92244c2354e7353157962e592e05b78e26569d
Grant Keebler <g072k474@ku.ed>
2020-10-20 21:04:20 -0500

    Fixed up the Html and started the typescript

--- dashboard/src/app/weight/weight.component.ts
+++ dashboard/src/app/weight/weight.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weight.component.css']
 })
 export class WeightComponent implements OnInit {
-
+  iniWeight: any;
+  goalWeight: any;
+  weightIn: any;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
+   
   }
+ updateInitial(): void
+ {
+  alert(this.iniWeight);
+ }
 
+ updateGoal(): void
+ {
+  alert(this.goalWeight);
+ }
+
+ updateCurrent(): void
+ {
+  alert(this.weightIn);
+ }
+ updateIni(event: any)
+ {
+   this.iniWeight=event.target.value;
+ }
+
+ updateG(event: any)
+ {
+   this.goalWeight=event.target.value;
+ }
+
+ updateC(event: any)
+ {
+   this.weightIn=event.target.value;
+ }
 }
