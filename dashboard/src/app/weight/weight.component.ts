d09785e300b9744cd54dd7b764202bb49dc10e85
Grant Keebler <g072k474@ku.ed>
2020-10-21 15:59:46 -0500

    Made lots of changes to HTMl and TS fully working

--- dashboard/src/app/weight/weight.component.ts
+++ dashboard/src/app/weight/weight.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeightComponent implements OnInit {
 
-  iniWeight: number;
-  goalWeight: number;
-  changedWeight: number;
-  differ: number;
-  remaining: number;
+  iniWeight: any;
+  goalWeight: any;
+  changedWeight: any;
+  differ: any;
+  remaining: any;
   commit:any; 
 
   constructor() { }
@@ -22,7 +22,7 @@ export class WeightComponent implements OnInit {
     this.goalWeight = 0;
     this.changedWeight = 0;
     this.differ = this.changedWeight - this.iniWeight; //actual weight increase, differ increase
-    this.remaining = this.changedWeight - this.goalWeight; //how much fat still need to lose
+    this.remaining = this.goalWeight - this.changedWeight; //how much fat still need to lose
     this.commit = '';
   }
 
@@ -44,30 +44,45 @@ export class WeightComponent implements OnInit {
   */
   doSubmit(): any{
     //goal reach
-    if(this.remaining < 0 ){
+    this.differ=this.changedWeight - this.iniWeight;
+    this.remaining=this.changedWeight-this.goalWeight;
+    if(this.remaining == 0 ){
       console.log("Congrats, you have reach the goal!");
     }
     //not reach the goal
     else{
       //get fat
       if(this.differ > 0){
-        console.log("you fat ");
-        console.log(this.differ);
-        console.log(" pounds");
+        console.log("You seemed to have gained weight " + this.differ + " lbs");
       }
       //
       else if(this.differ < 0 && this.remaining > 0){
-        console.log("you still need");
-        console.log(this.remaining);
-        console.log("pounds");
+        console.log("You still need to lose " + this.remaining + " lbs");
+      }
+      else if(this.remaining < 0)
+      {
+        console.log("You have gone past your goal Congradulations");
       }
       else{
-        console.log("your weight not change");
-        console.log("you still need");
-        console.log(this.remaining);
-        console.log("pounds");
+        console.log("Your weight has not changed.");
+        console.log("You still need to lose " + this.remaining + " lbs");
       }
     }
   }
+ updateIni(event: any)
+ {
+   this.iniWeight=event.target.value;
+ }
 
+ updateG(event: any)
+ {
+   this.goalWeight=event.target.value;
+ }
+
+ updateC(event: any)
+ {
+   this.changedWeight=event.target.value;
+ }
 }
+
+
