8fbd12e40aa068186a604106fcf4d172a9d6f05d
Runzhou Chen <runzhou@Runzhous-MacBook-Pro.local>
2020-10-22 03:07:20 +0800

    fix weight component

--- dashboard/src/app/weight/weight.component.ts
+++ dashboard/src/app/weight/weight.component.ts
@@ -7,9 +7,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeightComponent implements OnInit {
 
+  iniWeight: number;
+  goalWeight: number;
+  changedWeight: number;
+  differ: number;
+  remaining: number;
+  commit:any; 
+
+  constructor() { }
+
+  
+  ngOnInit(): void {
+    this.iniWeight = 0;
+    this.goalWeight = 0;
+    this.changedWeight = 0;
+    this.differ = this.changedWeight - this.iniWeight; //actual weight increase, differ increase
+    this.remaining = this.changedWeight - this.goalWeight; //how much fat still need to lose
+    this.commit = '';
+  }
+
+
+
+  /*
+  public iniWeight: 0;
+  public goalWeight: 0;
+  public changedWeight: 0;
+  public lowDiffer = this.iniWeight - this. changedWeight;  //when changedWeight lower than iniWeight
+  public highDiffer = this.changedWeight - this.iniWeight; //when changedWeight higher than iniWeigh
+  public remaining = this.changedWeight - this.goalWeight;
+  public commit: '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  */
+  doSubmit(): any{
+    //goal reach
+    if(this.remaining < 0 ){
+      console.log("Congrats, you have reach the goal!");
+    }
+    //not reach the goal
+    else{
+      //get fat
+      if(this.differ > 0){
+        console.log("you fat ");
+        console.log(this.differ);
+        console.log(" pounds");
+      }
+      //
+      else if(this.differ < 0 && this.remaining > 0){
+        console.log("you still need");
+        console.log(this.remaining);
+        console.log("pounds");
+      }
+      else{
+        console.log("your weight not change");
+        console.log("you still need");
+        console.log(this.remaining);
+        console.log("pounds");
+      }
+    }
   }
 
 }
